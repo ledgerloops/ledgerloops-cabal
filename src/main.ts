e15da6427f854342ae1eeee1fc9be3b637965367
Michiel de Jong <michiel@pondersource.com>
2024-05-15 14:03:01 +0200

    First message sent

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,17 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import Client from 'cabal-client';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const client = new Client({
+  config: {
+    dbdir: '/tmp/cabals'
+  }
+})
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+client.addCabal('cabal://411dbe21d6f8e222733ac88d2da3cf953aa9f5466db94fa9fa967a765be3875e').then((cabal) => {
+  console.log(cabal.key);
+  cabal.on('new-message', (event) => {
+    console.log(event.channel, event.author.key, event.message.value.content.text);
+  });
+  cabal.processLine('/join default');
+  cabal.processLine('hello cabal');
+  // resolves when the cabal is ready, returns a CabalDetails instance
+});
